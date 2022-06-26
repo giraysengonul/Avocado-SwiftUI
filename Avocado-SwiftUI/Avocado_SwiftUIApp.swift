7c044bf0c003ed18ad7104df848fffe6ec5a5f93
hakkicansengonul <coder.9734@gmail.com>
2022-06-26 19:57:13 +0300

    Initial Commit

---
+++ Avocado-SwiftUI/Avocado_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Avocado_SwiftUIApp.swift
+//  Avocado-SwiftUI
+//
+//  Created by hakkı can şengönül on 26.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct Avocado_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Avocado-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Avocado-SwiftUI
+//
+//  Created by hakkı can şengönül on 26.06.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
